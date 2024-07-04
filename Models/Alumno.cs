4ade233d4f8cff7c41925779365ccf59b1fb099d
saul basilio <saulbf96@outlook.es>
2024-07-03 19:03:14 -0600

    ok

---
+++ Models/Alumno.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public  class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre  { get; set; }
+        public string Email { get; set; }
+        public string  Foto { get; set; }
+        public int IdCurso { get; set; }
+        public  DateTime FechaAlta { get; set; }
+
+        public DateTime FechaBaja { get; set; }
+
+    }
+}
